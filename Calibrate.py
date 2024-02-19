1534c67e06cdc29e7e0fff84289c7e4ababfb90d
Michael Chestnut <72172361+MichaelChestnut@users.noreply.github.com>
2024-02-18 18:21:45 -0700

    Update Calibrate.py

--- Calibrate.py
+++ Calibrate.py
@@ -1,1 +1,9 @@
 # Calibration code coming soon
+
+print("Please follow these steps to calibrate the FermenTech digital hydrometer.")
+print("A physical hydrometer is necessary to calibrate this sensor")
+print("Insert the physical hydrometer into the brew")
+print("Read the hydrometer scale at the lowest point of the liquid's surface")
+print("Please input the physical hydrometer reading to three decimal places (example: 1.000)")
+print("\n")
+initial_specific_gravity = input("Input reading now: ")
