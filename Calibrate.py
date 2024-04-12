e5b06ab9f14c7a88a54dd094a736531fb131b23e
Michael Chestnut <72172361+MichaelChestnut@users.noreply.github.com>
2024-04-12 09:49:24 -0700

    Delete Calibrate.py

--- Calibrate.py
+++
@@ -1,8 +0,0 @@
-initial_specific_gravity = 0 # Initialize value to 0
-print("Please follow these steps to calibrate the FermenTech digital hydrometer.")
-print("A physical hydrometer is necessary to calibrate this sensor")
-print("Insert the physical hydrometer into the brew")
-print("Read the hydrometer scale at the lowest point of the liquid's surface")
-print("Please input the physical hydrometer reading to three decimal places (example: 1.000)")
-print("\n")
-initial_specific_gravity = input("Input reading now: ")
